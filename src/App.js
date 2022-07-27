e24c06d66dc0794d149bb2a54eefdbf7f22132ed
Luis Castillo <mr.luiscastillo@gmail.com>
2022-07-27 00:05:59 -0400

    fix conflict

--- src/App.js
+++ src/App.js
@@ -12,19 +12,14 @@ function App() {
       <ButtonGroup buttons={["One", "Two", "Three", "Four"]} />
       <Text />
       <Form />
-<<<<<<< HEAD
+
       <Counter />
       <Input2 />
-<<<<<<< HEAD
-=======
-      <Challenge />
->>>>>>> 4211829 (reactjs challange)
-=======
       <div>
         <h1>Child To Parent</h1>
         <Form />
       </div>
->>>>>>> 969f4f2 (practice passing data from child to parent)
+
     </div>
   );
 }
