8fd6ed9064fe70b3e9730979f21053b7f8a80c89
Luis Castillo <mr.luiscastillo@gmail.com>
2022-07-28 00:03:45 -0400

    add onChange function and console

--- src/components/FilterData/FilterData.jsx
+++ src/components/FilterData/FilterData.jsx
@@ -1,16 +1,20 @@
 import React from 'react'
 
 const FilterData = () =>{
+    const filterYearsData = (event) =>{
+        console.log("Filter Component")
+        console.log(event.target.value)
+    }
 
     return(
         <div>
             <div>
                 <label> Select Years</label>
-                <select>
-                    <option>2001</option>
-                    <option>2402</option>
-                    <option>2503</option>
-                    <option>2040</option>
+                <select onChange={filterYearsData}>
+                    <option value="2001">2001</option>
+                    <option value="2402">2402</option>
+                    <option value="2503">2503</option>
+                    <option value="2040">2040</option>
                 </select>
             </div>
         </div>
